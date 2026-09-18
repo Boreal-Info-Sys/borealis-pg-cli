bb8e99ddd20f53cecb1c912c6c0223ae89873305
Joel Andrews <joel.andrews@borealis-data.com>
2026-09-17 17:45:57 -0700

    Refactor line number workaround
    Switches from globby to tinyglobby since it is already included by updated dependencies. Also changes the line number workaround to a regular function rather than a faux Mocha test case.

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,8 +1,8 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import chaiString from 'chai-string'
-import globby from 'globby'
 import path from 'path'
+import {globSync} from 'tinyglobby'
 import {test as oclifTest} from '@oclif/test'
 
 process.env.BOREALIS_PG_ADDON_SERVICE_NAME = 'borealis-pg'
@@ -18,18 +18,19 @@ export const borealisPgApiBaseUrl = 'https://pg-heroku-addon-api.borealis-data.c
 
 // The following is a workaround for broken line number reporting in oclif commands (see
 // https://github.com/oclif/test/issues/50 and https://github.com/oclif/oclif/issues/314)
-test.stdout()
-  .stderr()
-  .timeout(15_000)
-  .it('performs the workaround for the oclif line number reporting bug', async () => {
-    const commandsDirPath = path.join(__dirname, 'commands')
-    const allTsFileNames = await globby('**/*.ts', {cwd: commandsDirPath})
-    const testFileNames = await globby('**/*.test.ts', {cwd: commandsDirPath})
+function fixBrokenLineNumbers() {
+  const commandsDirPath = path.join(__dirname, 'commands')
+  const allTsFileNames = globSync('**/*.ts', {cwd: commandsDirPath})
+  const testFileNames = globSync('**/*.test.ts', {cwd: commandsDirPath})
 
-    for (const filename of allTsFileNames) {
-      if (!testFileNames.includes(filename)) {
-        const commandPath = path.join(commandsDirPath, filename)
-        require(commandPath)
-      }
+  for (const filename of allTsFileNames) {
+    if (!testFileNames.includes(filename)) {
+      const commandPath = path.join(commandsDirPath, filename)
+
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require(commandPath)
     }
-  })
+  }
+}
+
+fixBrokenLineNumbers()
