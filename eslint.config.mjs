02aa8a6fb84c891e7c30854e6f3c39930deb0a6a
Joel Andrews <joel.andrews@borealis-data.com>
2026-09-17 18:09:06 -0700

    Update TypeScript config
    Various improvements to better work with Heroku CLI v13 and oclif v4.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-  globalIgnores(['/lib']),
+  globalIgnores(['dist']),
   {
     extends: tseslint.configs.recommended,
 
